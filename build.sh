77608ad51887892d9f04d605a84dee4423e95091
Tom Vercauteren <tomvercaut@gmail.com>
2023-08-22 23:41:21 +0200

    Eliminate docbook directory in source directory into build directory during the build.
    Updated both 'build.fish' and 'build.sh' scripts to create docbook directory within the existing build directory. Previously, docbook directories were being created in the project root. This created unnecessary clutter. This change improves project organization.

--- build.sh
+++ build.sh
@@ -18,14 +18,17 @@ transform_dicom_part() {
 
 download_deps() {
   # arg1: DICOM part
-  if [ ! -d docbook ]; then
-    mkdir docbook
+  if [ ! -d build ]; then
+    mkdir build
   fi
-  if [ ! -d "docbook/$1" ]; then
-    mkdir "docbook/$1"
+  if [ ! -d build/docbook ]; then
+    mkdir build/docbook
   fi
-  if [ ! -e "docbook/$1/$1.xml" ]; then
-    wget -O "docbook/$1/$1.xml" -c http://dicom.nema.org/medical/dicom/current/source/docbook/$1/$1.xml
+  if [ ! -d "build/docbook/$1" ]; then
+    mkdir "build/docbook/$1"
+  fi
+  if [ ! -e "build/docbook/$1/$1.xml" ]; then
+    wget -O "build/docbook/$1/$1.xml" -c http://dicom.nema.org/medical/dicom/current/source/docbook/$1/$1.xml
   fi
 }
 
@@ -37,7 +40,7 @@ rm -f build/*.xml
 
 download_deps part03
 download_deps part06
-transform_dicom_part build/registry.xml docbook/part06/part06.xml src/data_dictionary.xsl
-transform_dicom_part build/macros.xml docbook/part03/part03.xml src/macro_attributes.xsl
-transform_dicom_part build/modules.xml docbook/part03/part03.xml src/module_attributes.xsl
-transform_dicom_part build/ciod_modules.xml docbook/part03/part03.xml src/ciod_modules.xsl
+transform_dicom_part build/registry.xml build/docbook/part06/part06.xml src/data_dictionary.xsl
+transform_dicom_part build/macros.xml build/docbook/part03/part03.xml src/macro_attributes.xsl
+transform_dicom_part build/modules.xml build/docbook/part03/part03.xml src/module_attributes.xsl
+transform_dicom_part build/ciod_modules.xml build/docbook/part03/part03.xml src/ciod_modules.xsl
